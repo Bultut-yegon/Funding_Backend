753278643d6eae4323a1f1ab8747bdd7ccabbdb2
Bultut-yegon <bultut9785@gmail.com>
2026-07-05 00:13:05 +0300

    redeploy CORS fix

--- app/main.py
+++ app/main.py
@@ -74,6 +74,7 @@ app.add_middleware(
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
+    expose_headers=["*"],
 )
 
 app.include_router(auth.router, prefix="/api/auth", tags=["auth"])
